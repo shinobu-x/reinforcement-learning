df63471a9efc115f305a38b839e728175a3646ee
Shinobu KINJO <kinjo@sensetime.com>
2020-11-03 12:11:00 +0900

    modified:   acer.py

--- acer.py
+++ acer.py
@@ -3,7 +3,7 @@ from torch import nn
 
 class Actor(nn.Module):
     def __init__(self, state_space, action_space, hidden_space):
-        super(Actor).__init__()
+        super(Actor, self).__init__()
         self.state_space = state_space
         self.action_space = action_space
         self.hidden_space = hidden_space
@@ -22,6 +22,7 @@ class Actor(nn.Module):
 
 class Critic(nn.Module):
     def __init__(self, state_space, action_space, hidden_space):
+        super(Critic, self).__init__()
         self.state_space = state_space
         self.action_space = action_space
         self.hidden_space = hidden_space
@@ -38,8 +39,9 @@ class Critic(nn.Module):
         q = self.l3(x)
         return q # compute v later
 
-class Agent:
+class Agent(nn.Module):
     def __init__(self, state_space, action_space, hidden_space):
+        super(Agent, self).__init__()
         self.actor = Actor(state_space, action_space, hidden_space)
         self.critic = Critic(state_space, action_space, hidden_space)
 
@@ -50,21 +52,26 @@ class Agent:
         return distribution, Q, V, hidden_state
 
 class ACER(object):
-    def __init__(self, shared_model, shared_averaged_model, shared_optimizer,
-            state_space, action_space, hidden_space, timesteps = 100000,
-            done = True, forward_step = 100):
+    def __init__(self, env, shared_model, shared_average_model,
+            shared_optimizer, state_space, action_space, hidden_space,
+            replay_buffer, num_timesteps = 100000, done = True,
+            num_forward_steps = 100, num_episodes = 100):
+        self.env = env
         self.device = torch.device('gpu' if torch.cuda.is_available()
                 else 'cpu')
+        self.shared_model = shared_model
+        self.shared_average_model = shared_average_model
+        self.shared_optimizer = shared_optimizer
         self.state_space = state_space
         self.action_space = action_space
         self.hidden_space = hidden_space
+        self.replay_buffer = replay_buffer
         self.agent = Agent(self.state_space, self.action_space,
                 self.hidden_space).to(self.device)
-        self.timesteps = timesteps
+        self.num_timesteps = num_timesteps
         self.done = done
-        self.device = torch.device('gpu' if torch.cuda.is_available()
-                else 'gpu')
-        self.forward_steps = forward_steps
+        self.num_forward_steps = num_forward_steps
+        self.num_episodes = num_episodes
 
     def train(self):
         current_timestep = 1
@@ -75,8 +82,9 @@ class ACER(object):
                 average_hidden_state = torch.zeros(1, hidden_space)
                 current_state = torch.zeros(1, hidden_space)
                 average_current_state = torch.zeros(1, hidden_space)
-                state = torch.from_numpy(state).float().unsqueeze(0).to(
-                        self.device)
+                state = torch.from_numpy(self.env.reset())
+                self.done = False
+                current_episode = 0
             else:
                 hidden_state = hidden_state.detach()
                 current_state = current_state.detach()
@@ -89,3 +97,31 @@ class ACER(object):
             while not done and current_timestep <= self.forward_steps:
                 distribution, Q, V, (hidden_state, current_state) = \
                         agent(state, (hidden_state, current_state))
+                average_distribution, _, _, (average_hidden_state,
+                        average_current_state) = shared_average_model(state,
+                                (average_hidden_state, average_current_state))
+                action = torch.multinomial(distribution, 1)[0, 0]
+                next_state, reward, done, _ = env.step(action.item())
+                next_state = torch.from_numpy(next_state).float().to(
+                        self.device)
+                reward = min(max(reward, -1), 1)
+                self.done = self.done or current_episode >= self.num_episodes
+                current_episode += 1
+                if not self.enable_on_policy:
+                    self.replay_buffer.store(state, action, reward,
+                            distribution.detach())
+                distributions.append(distribution)
+                Qs.append(Q)
+                Vs.append(V)
+                actions.append(torch.LongTensor(action))
+                rewards.append(torch.Tensor(reward))
+                average_distributions.append(average_distribution)
+                current_timestep += 1
+                if done:
+                    Q = torch.zeros(1, 1)
+                    if not self.enable_on_policy:
+                        self.replay_buffer.store(state, None, None, None)
+                else:
+                    _, _, Q, _ = agent(state, (hidden_state, current_state))
+                    Q = Q.detach()
+
