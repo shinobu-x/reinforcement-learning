c7d27a7c98a2a3d82c7e5c699e3cd1805acf47c8
Shinobu KINJO <kinjo@sensetime.com>
2020-11-05 08:21:37 +0900

    new file:   utils/compute_gradient.py 	modified:   utils/compute_jacobian.py

---
+++ utils/compute_gradient.py
@@ -0,0 +1,12 @@
+import torch
+
+def compute_gradient(outputs, inputs, grad_outputs = None, retain_graph = None,
+        create_graph = False):
+    outputs = [outputs] if torch.is_tensor(outputs) else list(outputs)
+    inputs = [inputs] if torch.is_tensor(inputs) else list(inputs)
+    grads = torch.autograd.grad(outputs, inputs, grad_outputs,
+            allow_unused = True, retain_graph = retain_graph,
+            create_graph = create_graph)
+    grads = [grad if grad is not None else torch.zeros_like(input)
+            for grad, input in zip(grads, inputs)]
+    return torch.cat([grad.contiguous().view(-1) for grad in grads])
--- utils/compute_jacobian.py
+++ utils/compute_jacobian.py
@@ -1,15 +1,15 @@
-from itertools import product
 import torch
-from torch.autograd import grad
+from compute_gradient import compute_gradient
 
-def compute_jacobian(f, x):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    if f.dim() == 0: return grad(f, x)[0]
-    jacobian = torch.zeros(f.shape + x.shape).to(device)
-    grad_output = torch.zeros(*f.shape).to(device)
-    for index in product(*map(range, f.shape)):
-        grad_output[index] = 1
-        jacobian[index] = grad(f, x, grad_output = grad_output,
-                retain_graph = True)[0]
-        grad_output[index] = 0
-    return jacobian
+def compute_jacobian(outputs, inputs, create_graph = True):
+    outputs = [outputs] if torch.is_tensor(outputs) else list(outputs)
+    inputs = [inputs] if torch.is_tensor(inputs) else list(inputs)
+    jacobian = []
+    for output in outputs:
+        grad = torch.zeros_like(output.view(-1))
+        for i in range(len(output.view(-1))):
+            grad[i] = 1
+            jacobian += [compute_gradient(output.view(-1), inputs, grad,
+                retain_graph = True, create_graph = create_graph)]
+            grad[i] = 0
+    return torch.stack(jacobian)
