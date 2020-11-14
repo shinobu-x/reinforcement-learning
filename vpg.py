4109b4b2be430242e32188dc121017f091900dcf
Shinobu KINJO <kinjo@sensetime.com>
2020-11-14 11:57:43 +0900

    modified:   vpg.py

--- vpg.py
+++ vpg.py
@@ -38,7 +38,7 @@ class Critic(nn.Module):
 
 class VPG(object):
     def __init__(self, state_space, action_space, epsilon = 0.09, gamma = 0.99,
-            num_episodes = 100, num_timesteps = 10000):
+            num_batches = 32, num_episodes = 100, num_timesteps = 10000):
         self.device = torch.device('cuda' if torch.cuda.is_available()
                 else 'cpu')
         self.state_space = state_space
@@ -51,6 +51,7 @@ class VPG(object):
                 lr = 3e-4)
         self.epsilon = epsilon
         self.gamma = gamma
+        self.num_batches = num_batches
         self.num_episodes = num_episodes
         self.num_timesteps = num_timesteps
         self.history = []
@@ -62,24 +63,33 @@ class VPG(object):
         probabilities = self.policy(states)
         distribution = Categorical(probs = probabilities)
         action = distribution.sample()
-        log_probability = distribution.log_prob
+        log_probability = distribution.log_prob(action)
         return action, log_probability
 
+    def compute_loss(self, history):
+        returns = []
+        reward = 0.0
+        for r in history.reward:
+            reward += r * self.gamma * reward
+            returns.insert(0, reward)
+        returns = torch.FloatTensor(returns)
+        returns = (returns - returns.mean()) / (
+            returns.std() + np.finfo(np.float32).eps)
+        policy_loss = torch.mul(torch.FloatTensor(history.log_probability),
+                torch.autograd.Variable(returns) -
+                torch.autograd.Variable(torch.FloatTensor(history.value)))
     def train(self, env):
         for episode in range(self.num_episodes):
             episode_reward = 0.0
-            states = env.reset()
-            for timestep in range(self.num_timesteps):
-                action, log_probability = self.select_action(states)
-                value = self.critic(torch.autograd.Variable(torch.from_numpy(
-                    states).float()).to(self.device))
-                next_states, reward, done, _ = env.step(action.item())
-                self.history.append(self.log(log_probability, value, reward))
-            history = self.log(*zip(*self.history))
-
-import gym
-env = gym.make('CartPole-v1')
-state_space = env.observation_space.shape[0]
-action_space = env.action_space.n
-vpg = VPG(state_space, action_space)
-vpg.train(env)
+            for batch in range(self.num_batches):
+                states = env.reset()
+                for timestep in range(self.num_timesteps):
+                    action, log_probability = self.select_action(states)
+                    value = self.critic(torch.autograd.Variable(
+                        torch.from_numpy(states).float()).to(self.device))
+                    next_states, reward, done, _ = env.step(action.item())
+                    self.history.append(self.log(
+                        log_probability, value, reward))
+                history = self.log(*zip(*self.history))
+                episode_reward += np.sum(history.reward) / self.num_batches
+                self.compute_loss(history)
